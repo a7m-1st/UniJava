6ad988f9e0308aebd6f23e62ac3957630a048a5f
A7M1ST <A7M1ST@LAPTOP-FC9PFB0E>
2022-11-17 17:04:15 +0800

    first commit

---
+++ L1Q2/src/l1q2/MyProfile.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package l1q2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author A7M1ST
+ */
+public class MyProfile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner x = new Scanner(System.in);
+        
+        System.out.println("This is my matric number: " + x.nextLine());
+    }
+    
+}
